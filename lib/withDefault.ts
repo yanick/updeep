cbb390f95bf82783f97a6b826c2178502ee26b34
Yanick Champoux <yanick@babyl.dyndns.org>
2019-01-18 18:20:04 -0500

    withDefault

--- lib/withDefault.ts
+++ lib/withDefault.ts
@@ -1,7 +1,9 @@
 import update from './update'
-import curry from 'lodash/curry'
+import curry from './util/curry'
+import { UpdateReturnType, MergedUpdate } from './types';
 
-function withDefault(defaultValue: object, updates: any, object: any) {
+function withDefault<D,U,O>(defaultValue: D,updates: U, object: O): MergedUpdate<U,O extends null|undefined ? D : O>;
+function withDefault(defaultValue: any, updates: any, object: any):any {
   if (typeof object === 'undefined') {
     return update(updates, defaultValue)
   }
@@ -9,4 +11,9 @@ function withDefault(defaultValue: object, updates: any, object: any) {
   return update(updates, object)
 }
 
-export default curry(withDefault)
+interface CurriedWithDefault {
+    <D,U,O  extends any>(defaultValue: D,updates: U, object: O): MergedUpdate<U,O extends null|undefined ? D : O>;
+    <D,U>(defaultValue: D, updates: U ): <O>( obj: O) => MergedUpdate<U,O extends null|undefined ? D : O>;
+}
+
+export default curry(withDefault) as CurriedWithDefault;
--- types/withDefault.d.ts
+++ types/withDefault.d.ts
@@ -1,2 +1,7 @@
-declare const _default: import("lodash").CurriedFunction3<object, any, any, any>;
+import { MergedUpdate } from './types';
+interface CurriedWithDefault {
+    <D, U, O extends any>(defaultValue: D, updates: U, object: O): MergedUpdate<U, O extends null | undefined ? D : O>;
+    <D, U>(defaultValue: D, updates: U): <O>(obj: O) => MergedUpdate<U, O extends null | undefined ? D : O>;
+}
+declare const _default: CurriedWithDefault;
 export default _default;
