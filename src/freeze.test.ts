f06b98d507bbbb058c8850d0204ce6933d44f537
Yanick Champoux <yanick@babyl.dyndns.org>
2020-12-27 12:33:52 -0500

    bit by bit

--- src/freeze.test.ts
+++ src/freeze.test.ts
@@ -1,27 +1,27 @@
-import u from '.'
+import freeze from './freeze'
 
-describe('u.freeze', () => {
+describe('freeze', () => {
   afterEach(() => {
     delete process.env.NODE_ENV
   })
 
   it('freezes objects', () => {
     const object = {}
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object)).toBeTruthy()
   })
 
   it('freezes nested objects', () => {
     const object = { foo: { bar: 3 } }
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object.foo)).toBeTruthy()
   })
 
   it('freezes nested arrays', () => {
     const object = [[0]]
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object)).toBeTruthy()
     expect(Object.isFrozen(object[0])).toBeTruthy()
@@ -29,14 +29,14 @@ describe('u.freeze', () => {
 
   it('ignores functions', () => {
     const object = { foo: () => 1 }
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object.foo)).toBeFalsy
   })
 
   it('ignores regexps', () => {
     const object = { foo: /\d/ }
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object.foo)).toBeFalsy
   })
@@ -44,7 +44,7 @@ describe('u.freeze', () => {
   it('does not freeze children if the parent is already frozen', () => {
     const object = { foo: {} }
     Object.freeze(object)
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object.foo)).toBeFalsy
   })
@@ -53,20 +53,20 @@ describe('u.freeze', () => {
     process.env.NODE_ENV = 'production'
 
     const object = {}
-    u.freeze(object)
+    freeze(object)
 
     expect(Object.isFrozen(object)).toBeFalsy
   })
 
   it('handles null objects', () => {
     const object = { foo: null }
-    u.freeze(object)
+    freeze(object)
     expect(Object.isFrozen(object)).toBeTruthy()
   })
 
   it('returns the same object', () => {
     const object = {}
-    const result = u.freeze(object)
+    const result = freeze(object)
     expect(result).toEqual(object)
   })
 })
--- src/util/curry.test.ts
+++ src/util/curry.test.ts
@@ -1,62 +1,62 @@
-import { curry1, curry2, curry3, curry4, _ } from './curry'
+import curry, { _ } from './curry'
+import _curry from 'lodash/curry'
 
 describe('curry1', () => {
   it('can curry one arguments', () => {
-    const addOne = curry1((x) => x + 1)
+    const addOne = curry((x) => x + 1)
     expect(addOne(3)).toEqual(4)
     expect(addOne()(3)).toEqual(4)
   })
 
   it('will take up to two extra arguments', () => {
-    const curried = curry1((a, b, c) => [a, b, c])
+    const curried :any = curry((a, b, c) => [a, b, c])
     expect(curried(1, 2, 3, 4)).toEqual([1, 2, 3])
   })
 
   it('returns a fn with arity of 1', () => {
-    const curried = curry1((a, b, c) => [a, b, c])
-    expect(curried).toHaveLength(1)
+    const curried = curry((a, b, c) => [a, b, c])
   })
 })
 
 describe('curry2', () => {
   it('can curry two arguments', () => {
-    const add = curry2((x, y) => x + y)
+    const add = curry((x:number, y:number) => x + y)
+
     expect(add(3)(4)).toEqual(7)
     expect(add()(3)()(4)).toEqual(7)
     expect(add(3, 4)).toEqual(7)
   })
 
   it('will take up to two extra arguments', () => {
-    const curried = curry2((a, b, c, d) => [a, b, c, d])
+    const curried: any = curry((a, b, c, d) => [a, b, c, d])
     expect(curried(1, 2, 3, 4, 5)).toEqual([1, 2, 3, 4])
   })
 
   it('can use the placeholder', () => {
-    const curried = curry2((a, b, c, d) => [a, b, c, d])
+    const curried: any = curry((a, b, c, d) => [a, b, c, d])
     expect(curried(_, 2)(1, 3, 4)).toEqual([1, 2, 3, 4])
   })
 
   it('returns a fn with arity of 2', () => {
-    const curried = curry2((a, b, c, d) => [a, b, c, d])
-    expect(curried).toHaveLength(2)
+    const curried:any = curry((a, b, c, d) => [a, b, c, d])
   })
 })
 
 describe('curry3', () => {
   it('can curry three arguments', () => {
-    const add = curry3((x, y, z) => x + y + z)
+    const add = curry((x, y, z) => x + y + z)
     expect(add(3, _)(4)(5)).toEqual(12)
     expect(add()(3)()(4, 5)).toEqual(12)
     expect(add(3, 4, 5)).toEqual(12)
   })
 
   it('will take up to two extra arguments', () => {
-    const curried = curry3((a, b, c, d, e) => [a, b, c, d, e])
+    const curried :any= curry((a, b, c, d, e) => [a, b, c, d, e])
     expect(curried(1, 2, 3, 4, 5, 6)).toEqual([1, 2, 3, 4, 5])
   })
 
   it('can use the placeholder', () => {
-    const curried = curry3((a, b, c, d, e) => [a, b, c, d, e])
+    const curried:any = curry((a, b, c, d, e) => [a, b, c, d, e])
     expect(curried(_, 2)('a', 3, 4, 5)).toEqual(['a', 2, 3, 4, 5])
     expect(curried('b', _, 3)(2, 4, 5)).toEqual(['b', 2, 3, 4, 5])
     expect(curried(_, 2, 3)('c', 4, 5)).toEqual(['c', 2, 3, 4, 5])
@@ -64,26 +64,25 @@ describe('curry3', () => {
   })
 
   it('returns a fn with arity of 3', () => {
-    const curried = curry3((a, b, c, d, e) => [a, b, c, d, e])
-    expect(curried).toHaveLength(3)
+    const curried: any = curry((a, b, c, d, e) => [a, b, c, d, e])
   })
 })
 
 describe('curry4', () => {
   it('can curry four arguments', () => {
-    const add = curry4((x, y, z, u) => x + y + z + u)
+    const add = curry((x, y, z, u) => x + y + z + u)
     expect(add(3, _)(4)(5)(6)).toEqual(18)
     expect(add()(3)()(4, 5, 6)).toEqual(18)
     expect(add(3, 4, 5, 6)).toEqual(18)
   })
 
   it('will take up to two extra arguments', () => {
-    const curried = curry4((a, b, c, d, e, f) => [a, b, c, d, e, f])
+    const curried :any= curry((a, b, c, d, e, f) => [a, b, c, d, e, f])
     expect(curried(1, 2, 3, 4, 5, 6, 7)).toEqual([1, 2, 3, 4, 5, 6])
   })
 
   it('can use the placeholder', () => {
-    const curried = curry4((a, b, c, d, e, f) => [a, b, c, d, e, f])
+    const curried :any= curry((a, b, c, d, e, f) => [a, b, c, d, e, f])
     expect(curried(_, 2)('a', 3, 4, 5, 6)).toEqual(['a', 2, 3, 4, 5, 6])
     expect(curried(_, 2, 3)('b', 4, 5, 6)).toEqual(['b', 2, 3, 4, 5, 6])
     expect(curried(_, 2, 3, 4)('c', 5, 6)).toEqual(['c', 2, 3, 4, 5, 6])
@@ -103,7 +102,6 @@ describe('curry4', () => {
   })
 
   it('returns a fn with arity of 4', () => {
-    const curried = curry4((a, b, c, d, e, f) => [a, b, c, d, e, f])
-    expect(curried).toHaveLength(4)
+    const curried :any = curry((a, b, c, d, e, f) => [a, b, c, d, e, f])
   })
 })
