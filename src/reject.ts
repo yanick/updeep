fae3873726c4c9d2efe0644fdfd9b9ca03afe8e1
Yanick Champoux <yanick@babyl.dyndns.org>
2020-12-27 23:35:19 -0500

    wip reject

--- src/reject.ts
+++ src/reject.ts
@@ -1,16 +1,22 @@
 import _reject from 'lodash/reject'
 import wrap, {Wrap} from './wrap'
 
-export type Reject<C extends any> =
-    Wrap< [ unknown,  C[] ], C[] >;
+interface CurriedReject {
+    <C extends any[]>(predicate: any, collection: C): C;
+    <C extends object>(predicate: any, collection: C): Array< C[ keyof C ] >
+        ;
 
-export function reject<C>(predicate: unknown, collection: C[]): C[] {
+    <C extends any[]>(predicate: any ): ( collection: C) => C;
+    <C extends object>(predicate: any):( collection: C) => Array< C[ keyof C ] >
+        ;
+}
+
+
+function reject(predicate: any, collection: any) {
   const result = _reject(collection, predicate as any)
   const equal = collection.length === result.length
 
   return equal ? collection : result
 }
 
-export default wrap(reject) as Wrap<
-    Parameters<typeof reject>, ReturnType<typeof reject>
->;
+export default wrap(reject) as CurriedReject;
